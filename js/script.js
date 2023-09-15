13989c412f7cc97b25a20111fe714147ec75469b
amandajones1996 <jonesamanda996@gmail.com>
2023-09-14 23:20:34 -0400

    functions implemented

--- js/script.js
+++ js/script.js
@@ -17,7 +17,36 @@ For assistance:
 Create the `showPage` function
 This function will create and insert/append the elements needed to display a "page" of nine students
 */
+function showPage(list, page){
+   const itemsPerPage = 9;
 
+   startIndex = (page * itemsPerPage) - itemsPerPage
+   endIndex = page * itemsPerPage
+
+   const studentList = document.querySelector(".student-list")
+
+   studentList.innerHTML = '';
+   
+   for(let i = 0; i < list.length; i++){
+      if (i >= startIndex && i < endIndex){
+         const student = data[i]
+         
+         const studentItem =`
+               <li class="student-item cf">
+                  <div class="student-details">
+                     <img class="avatar" src="https://randomuser.me/api/portraits/women/25.jpg" alt="Profile Picture">
+                     <h3>${student.name.first} ${student.name.last}</h3>
+                     <span class="email">${student.email}</span>
+                  </div>
+                     <div class="joined-details">
+                     <span class="date">${student.registered.date}</span>
+                  </div>
+            </li>
+         `;
+         studentList.insertAdjacentHTML('beforeend', studentItem)
+      }
+   }
+}
 
 
 /*
@@ -25,6 +54,34 @@ Create the `addPagination` function
 This function will create and insert/append the elements needed for the pagination buttons
 */
 
+function addPagination(list){
+   const pagesNeeded = Math.ceil(list.length / 9)
+   const linkList = document.querySelector(".link-list")
+   linkList.innerHTML = ''
 
+   for(let i = 1; i <= pagesNeeded; i++){
+      const button = `
+      <li>
+            <button>${i}</button>
+      </li>
+      `
+      linkList.insertAdjacentHTML('beforeend', button)
+   }
+   const firstButton = linkList.querySelector('button')
+   firstButton.className = 'active'
+
+   linkList.addEventListener("click", (e) => {
+      if (e.target.tagName === 'BUTTON'){
+         for(const button of linkList.querySelectorAll('button')){
+            button.classList.remove('active')
+         }
+         e.target.classList.add('active')
+         const page = parseInt(e.target.textContent)
+         showPage(list, page)
+      }
+   })
+}
 
 // Call functions
+showPage(data, 1);
+addPagination(data)
